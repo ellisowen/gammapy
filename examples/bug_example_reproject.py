56f536cfa398bd202943a79fe0f5351523a94def
ellisowen <ellis.owen@mpi-hd.mpg.de>
2014-07-17 19:07:58 +0200

    update

--- examples/bug_example_reproject.py
+++ examples/bug_example_reproject.py
@@ -1,6 +1,8 @@
 from FITS_tools.cube_regrid import regrid_cube_hdu
+from gammapy.datasets import FermiGalacticCenter
+from astropy.io import fits
 
 diffuse = fits.open(FermiGalacticCenter.filenames()['diffuse_model'])[0]
 exposure = fits.open(FermiGalacticCenter.filenames()['exposure_cube'])[0]
 
-regrid_cube_hdu(hdu1, hdu2.header, smooth=True)
+regrid_cube_hdu(exposure, diffuse.header, smooth=False)
