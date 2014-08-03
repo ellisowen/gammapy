68739706089371004b839d542130331da8458001
eowen <ellis.owen@mpi-hd.mpg.de>
2014-08-03 18:37:06 +0200

    updage

--- gammapy/image/catalog.py
+++ gammapy/image/catalog.py
@@ -6,12 +6,16 @@ __all__ = ['reference_image', 'catalog_image']
 from astropy.io import fits
 from astropy.modeling.models import Gaussian2D, Disk2D
 from gammapy.image.utils import make_header, disk_correlate
-from gammapy.datasets.load import fetch_fermi_extended_sources
+from gammapy.datasets.load import fetch_fermi_extended_sources, fetch_fermi_catalog
+from gammapy.datasets import FermiGalacticCenter
 from gammapy.spectral_cube import GammaSpectralCube
 from gammapy.image import coordinates
 from astropy.wcs import WCS
 from astropy.units import Quantity
+from astropy.table import Table
+from gammapy.irf import EnergyDependentTablePSF
 import numpy as np
+from astropy.coordinates import Angle
 
 
 def reference_image(resolution, latitudes, longitudes, center=[0,0], units='ph/cm2/s/sr'):
@@ -33,35 +37,31 @@ def catalog_image(psf='Fermi', resolution=0.1, center=[0, 0], lat_range=[0, 180]
     """
     reference = reference_image(resolution, lat_range, lon_range, center, units='ph/cm2/s/sr') #Check these units!
     lons, lats = coordinates(reference)
-    if psf == 'None':
-        # If there is no PSF defined, sources will be modeled as Gaussian
-        source_kernel = Gaussian2D(0, 0, 0, 0.1, 0.1)
-        new_image = im_1 = im_2 = im_3 = source_kernel(lats, lons)
-    else:
-        # Otherwise, all of the flux will be placed into the reference pixel to be later PSF convolved with the defined PSF
-        # Hence original reference empty image is called
-        new_image = im_1 = im_2 = im_3 = reference.data
     wcs = WCS(reference.header)
-    total_point_image = GammaSpectralCube(data = new_image, wcs = wcs, energy = energy)
+    total_point_image = GammaSpectralCube(data = Quantity(np.array([reference.data]), ''), wcs = wcs, energy = energy)
     new_image = np.zeros_like(total_point_image.data, dtype=np.float64)
-    if source_type == 'ExtendedSource' or 'All':
-        hdu_list = fetch_fermi_extended_sources(catalog)
+    if source_type == 'ExtendedSource':# or 'All':
+        # Note that the first extended source fits file is unreadable...
+        hdu_list = fetch_fermi_extended_sources(catalog)[1:]
         for source in hdu_list:
             source_wcs = WCS(source.header)
-            source_spec_cube = GammaSpectralCube(data = source.data, wcs=source_wcs, energy=energy)
+            source_spec_cube = GammaSpectralCube(data = Quantity(np.array([source.data]), ''), wcs=source_wcs, energy=energy)
             new_source_cube = source_spec_cube.reproject_to(total_point_image)
             total_point_image.data = total_point_image.data + np.nan_to_num(new_source_cube.data)
-    elif source_type == 'PointSource' or 'All':
-        source_table = catalog_table(catalog, extended='No')
-        source_list = np.arange(len(source_table['Flux'])).astype(int)
-        for source in source_list:
+            extended_image = total_point_image
+    elif source_type == 'PointSource':# or 'All':
+        source_table = catalog_table(catalog, ebands='No')
+        for source in np.arange(len(source_table['Flux'])):
             lon = source_table['GLON'][source]
             lat = source_table['GLAT'][source]
             flux = source_table['Flux'][source]
             x, y = wcs.wcs_world2pix(lon, lat, 0)
             xi, yi = x.astype(int), y.astype(int)
-            new_image[yi, xi] += flux
-    total_point_image = GammaSpectralCube(data = new_image, wcs = wcs, energy = energy)
+            new_image[0][yi, xi] = new_image[0][yi, xi] + flux
+        ptsrc_image = total_point_image
+    #if source_type == 'All':
+    #    combined_data = extended_image.data + ptsrc_image.data
+    #    total_point_image = GammaSpectralCube(data = combined_data, wcs = wcs, energy = energy)
     if psf == 'Gaussian':
         from scipy.ndimage import gaussian_filter
         new_image = gaussian_filter(new_image, 2)
@@ -72,15 +72,19 @@ def catalog_image(psf='Fermi', resolution=0.1, center=[0, 0], lat_range=[0, 180]
         out_cube = GammaSpectralCube(data=new_image, wcs=total_point_image.wcs,
                                      energy=energy)
     elif psf == 'Fermi':
+        from scipy.ndimage import convolve
         psf_object = EnergyDependentTablePSF.read(FermiGalacticCenter.filenames()['psf'])
+        convolved_cube = new_image.copy()
         for i in np.arange(len(energy) - 1):
             psf = psf_object.table_psf_in_energy_band(Quantity([energy[i].value,
                                                             energy[i + 1].value],
                                                            energy.unit))
-            kernel_array = psf.kernel(pixel_size, offset_max)
+            # Previous codes generally seem to show 5 pixel max-offset is fine
+            # TODO: offer these as kwargs to the user...
+            kernel_array = psf.kernel(Angle(resolution, 'deg'), Angle(5, 'deg'))
             kernel_image = kernel_array / kernel_array.sum()
-            convolved_cube[i] = convolve(new_image, kernel_image,
-                                         mode='constant')
+            convolved_cube[i] = convolve(new_image[i], kernel_image,
+                                            mode='constant')
         out_cube = GammaSpectralCube(data=convolved_cube, wcs=total_point_image.wcs,
                                      energy=energy)
     
@@ -96,32 +100,34 @@ def catalog_table(catalog, ebands='No'):
     """
     from scipy.stats import gmean
     data = []
-    for source in np.arange(len(fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog'))):
-        glon = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['GLON'][source]
-        glat = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['GLAT'][source]
-        source_name = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Source_Name'][source]
+    cat_table = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')
+    for source in np.arange(len(cat_table)):
+        glon = cat_table['GLON'][source]
+        glat = cat_table['GLAT'][source]
+        source_name = cat_table['Source_Name'][source]
         # Different from here between each catalog because of different catalog header names
         if catalog == '1FHL':
             energy = Quantity([10, 30, 100, 500], 'GeV')
             if ebands == 'No':
-                flux_bol = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux'][source]
+                flux_bol = cat_table['Flux'][source]
+                row = dict(Source_Type='PointSource', Source_Name=source_name, GLON=glon, GLAT=glat, Flux=flux_bol)
             else:
-                Flux_10_30 = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux10_30GeV'][source]
-                Flux_30_100 = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux30_100GeV'][source]
-                Flux_100_500 = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux100_500GeV'][source]
+                Flux_10_30 = cat_table['Flux10_30GeV'][source]
+                Flux_30_100 = cat_table['Flux30_100GeV'][source]
+                Flux_100_500 = cat_table['Flux100_500GeV'][source]
                 row = dict(Source_Type='PointSource', Source_Name=source_name, GLON=glon, GLAT=glat, Flux10_30=Flux10_30, Flux30_100=Flux30_100, Flux100_500=Flux100_500)
         elif catalog == '2FGL':
             energy = Quantity([30, 100, 300, 1000, 3000, 10000, 100000], 'GeV') 
             if ebands == 'No':
-                flux_bol = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux_Density'][source]
+                flux_bol = cat_table['Flux_Density'][source]
                 row = dict(Source_Type='PointSource', Source_Name=source_name, GLON=glon, GLAT=glat, Flux=flux_bol)
             else:
-                Flux_30_100 = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux30_100'][source]
-                Flux_100_300  = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux100_300'][source]
-                Flux_300_1000  = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux300_1000'][source]
-                Flux_1000_3000  = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux1000_3000'][source]
-                Flux_3000_10000  = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux3000_10000'][source]
-                Flux_10000_100000  = fetch_fermi_catalog(catalog, 'LAT_Point_Source_Catalog')['Flux10000_100000'][source]
+                Flux_30_100 = cat_table['Flux30_100'][source]
+                Flux_100_300  = cat_table['Flux100_300'][source]
+                Flux_300_1000  = cat_table['Flux300_1000'][source]
+                Flux_1000_3000  = cat_table['Flux1000_3000'][source]
+                Flux_3000_10000  = cat_table['Flux3000_10000'][source]
+                Flux_10000_100000  = cat_table['Flux10000_100000'][source]
                 row = dict(Source_Type='PointSource', Source_Name=source_name, GLON=glon, GLAT=glat, Flux_30_100=Flux_30_100, Flux_100_300=Flux_100_300, Flux_300_1000=Flux_300_1000, Flux_1000_3000=Flux_1000_3000, Flux_3000_10000=Flux_3000_10000, Flux_10000_100000=Flux_10000_100000)
         data.append(row)
     table = Table(data)
@@ -130,4 +136,4 @@ def catalog_table(catalog, ebands='No'):
 
 if __name__ == '__main__':
     catalog_image(psf='Fermi', resolution=0.1, center=[0, 0], lat_range=[0, 180], lon_range=[0, 360],
-                  catalog='1FHL', source_type='All', total_flux='False', filename='1fhl_fermi_psf.fits', energy = Quantity([10, 500], 'GeV'))
+                  catalog='1FHL', source_type='PointSource', total_flux='False', filename='1fhl_fermi_psf.fits', energy = Quantity([10, 500], 'GeV'))
