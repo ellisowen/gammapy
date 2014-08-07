700c95698269ed2fbe68d0a62746d3609ca9dcf7
eowen <ellis.owen@mpi-hd.mpg.de>
2014-08-07 18:14:56 +0200

    updates plotting scripts

--- docs/tutorials/npred/npred_convolved.py
+++ docs/tutorials/npred/npred_convolved.py
@@ -3,49 +3,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy.units import Quantity
-from astropy.io import fits
-from astropy.wcs import WCS
-from gammapy.spectral_cube import (compute_npred_cube, GammaSpectralCube,
-                                   convolve_npred_cube)
-from gammapy.datasets import FermiVelaRegion
-from gammapy.irf import EnergyDependentTablePSF
 
-# Reads in data
-background_file = FermiVelaRegion.filenames()['diffuse_model']
-exposure_file = FermiVelaRegion.filenames()['exposure_cube']
-counts_file = FermiVelaRegion.filenames()['counts_cube']
-background_model = GammaSpectralCube.read(background_file)
-exposure_cube = GammaSpectralCube.read(exposure_file)
+from npred_general import prepare_images
 
-# Reproject background cube
-repro_bg_cube = background_model.reproject_to(exposure_cube)
-
-# Define energy band required for output
-energies = Quantity([10, 500], 'GeV')
-
-# Compute the predicted counts cube
-npred_cube = compute_npred_cube(repro_bg_cube, exposure_cube, energies)
-
-# Convolve with Energy-dependent Fermi LAT PSF
-psf = EnergyDependentTablePSF.read(FermiVelaRegion.filenames()['psf'])
-convolved_npred_cube = convolve_npred_cube(npred_cube, psf, 3, 1)
-
-# Counts data
-counts_data = fits.open(counts_file)[0].data
-counts_wcs = WCS(fits.open(counts_file)[0].header)
-counts_cube = GammaSpectralCube(data=Quantity(counts_data, ''), wcs=counts_wcs,
-                                energy=energies)
-counts_cube = counts_cube.reproject_to(npred_cube)
-
-counts = counts_cube.data[0]
-model = convolved_npred_cube.data[0]
-
-# Load Fermi tools gtmodel result
-gtmodel = fits.open(FermiVelaRegion.filenames()['background_image'])[0].data.astype(float)
-
-# Ratio for the two background images
-ratio = np.nan_to_num(model / gtmodel)
+model, gtmodel, ratio, counts = prepare_images()
 
 # Plotting
 vmin, vmax = 0, 1
