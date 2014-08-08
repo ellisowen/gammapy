bd95b5de6bc7332b4485147bc433335839079d80
eowen <ellis.owen@mpi-hd.mpg.de>
2014-08-08 16:18:14 +0200

    Adds predicted counts cube tutorial, including plotting scripts and updates doc index.rst file to accomodate new addition

---
+++ docs/tutorials/npred/npred_convolved.py
@@ -0,0 +1,35 @@
+"""Runs commands to produce convolved predicted counts map in current directory
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from npred_general import prepare_images
+
+model, gtmodel, ratio, counts = prepare_images()
+
+# Plotting
+vmin, vmax = 0, 1
+
+fig, axes = plt.subplots(nrows=1, ncols=3)
+
+results = [model, gtmodel, ratio]
+titles = ['Gammapy Background', 'Fermi Tools Background', 'Ratio: \n Gammapy/Fermi Tools']
+
+for i in np.arange(3):
+    im = axes.flat[i].imshow(results[i],
+                             interpolation='nearest',
+                             origin="lower", vmin=vmin, vmax=vmax,
+                             cmap=plt.get_cmap())
+
+    axes.flat[i].set_title(titles[i], fontsize=12)
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.3, 0.025, 0.4])
+fig.colorbar(im, cax=cbar_ax)
+a = fig.get_axes()[0]
+b = fig.get_axes()[1]
+c = fig.get_axes()[2]
+a.set_axis_off()
+b.set_axis_off()
+c.set_axis_off()
