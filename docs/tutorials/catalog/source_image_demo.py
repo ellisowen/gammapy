6e5e61b8a521d7986ddd88d8610a8c841c5f152e
eowen <ellis.owen@mpi-hd.mpg.de>
2014-08-09 21:19:01 +0200

    update

--- docs/tutorials/catalog/source_image_demo.py
+++ docs/tutorials/catalog/source_image_demo.py
@@ -1,14 +1,12 @@
-from gammapy.image.catalog import catalog_image
-from gammapy.image import make_empty_image
-from gammapy.irf import EnergyDependentTablePSF
-from gammapy.datasets import FermiGalacticCenter
-from astropy.io import fits
-import numpy as np
+"""Produces an image from 1FHL catalog point sources.
+"""
 import matplotlib.pyplot as plt
+from gammapy.datasets import FermiGalacticCenter
+from gammapy.image import make_empty_image
+from gammapy.image.catalog import catalog_image
+from gammapy.irf import EnergyDependentTablePSF
 
-
-psf_file = FermiGalacticCenter.filenames()['psf']
-
+# Define image size
 nxpix = 1000
 nypix = 1000
 binsz = 0.1
@@ -16,8 +14,11 @@ binsz = 0.1
 reference = make_empty_image(nxpix, nypix, binsz)
 psf = EnergyDependentTablePSF.read(psf_file)
 
+# Create image
 image = catalog_image(reference, psf, catalog='1FHL', source_type = 'point',
                   total_flux='True')
 image_hdus = image.to_fits()
 
-image_hdu = image_hdus[0]
+# Plot
+image = image_hdus[0].data
+plt.imshow(image)
--- gammapy/image/__init__.py
+++ gammapy/image/__init__.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Image processing and analysis tools."""
+from .catalog import *
 from .healpix import *
 from .measure import *
 from .profile import *
